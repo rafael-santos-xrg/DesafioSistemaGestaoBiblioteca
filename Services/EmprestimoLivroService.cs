8de7a390a6b0377e14bb8f36ef338039bd0b201a
victor-santos-xrg <victorsantos.pba@outlook.com>
2025-03-24 20:07:03 -0300

    Cria o contrato e a implementação do emprestimo de um item

---
+++ Services/EmprestimoLivroService.cs
@@ -0,0 +1,17 @@
+﻿using DesafioSistemaGestaoBiblioteca.Models;
+
+namespace DesafioSistemaGestaoBiblioteca.Services
+{
+    internal class EmprestimoLivroService : IEmprestimoItemService
+    {
+        public Emprestimo Empresta(Guid idLivro, Visitante visitante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Devolve(Guid idEmprestimo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IEmprestimoItemService.cs
@@ -0,0 +1,11 @@
+﻿using DesafioSistemaGestaoBiblioteca.Models;
+
+namespace DesafioSistemaGestaoBiblioteca.Services
+{
+    internal interface IEmprestimoItemService
+    {
+        Emprestimo Empresta(Guid idItem, Visitante visitante);
+
+        void Devolve(Guid idEmprestimo);
+    }
+}
