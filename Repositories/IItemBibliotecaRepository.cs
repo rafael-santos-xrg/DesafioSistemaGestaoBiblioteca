3f1b1f6ba1d4827374562535c2dd086f7090199b
victor-santos-xrg <victorsantos.pba@outlook.com>
2025-03-24 20:08:19 -0300

    Cria o contrato de items da biblioteca

---
+++ Repositories/IItemBibliotecaRepository.cs
@@ -0,0 +1,14 @@
+﻿using DesafioSistemaGestaoBiblioteca.Models;
+
+namespace DesafioSistemaGestaoBiblioteca.Repositories
+{
+    internal interface IItemBibliotecaRepository<T> where T : ItemBiblioteca
+    {
+        void Adicionar(T itemBiblioteca);
+        void Atualizar(T itemBiblioteca);
+        void Remover(T itemBiblioteca);
+        IEnumerable<T> BuscarTodos();
+        T BuscarPorId(Guid itemId);
+
+    }
+}
