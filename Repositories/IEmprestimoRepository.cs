2bf7877af80f2bf5d693b76e499132c96b4bc996
victor-santos-xrg <victorsantos.pba@outlook.com>
2025-04-09 22:40:07 -0300

    Adiciona IEmprestimoRepository

---
+++ Repositories/IEmprestimoRepository.cs
@@ -0,0 +1,13 @@
+﻿using DesafioSistemaGestaoBiblioteca.Models;
+
+namespace DesafioSistemaGestaoBiblioteca.Repositories
+{
+    internal interface IEmprestimoRepository<T> where T : ItemBiblioteca
+
+    {
+    void Adicionar(Emprestimo<T> emprestimo);
+    void Remover(Guid emprestimoId);
+    List<Emprestimo<T>> BuscarTodos();
+    Emprestimo<T> BuscaPorId(Guid emprestimoId);
+    }
+}
