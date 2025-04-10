4609a40d25d347f102d3fc567eb42d7613c3fa0f
victor-santos-xrg <victorsantos.pba@outlook.com>
2025-04-09 22:27:29 -0300

    Muda construção do Livro quanto ao ISBN, adiciona o atributo NumPaginas e adiciona o método CalculaPrazoEmprestimo

--- Models/Livro.cs
+++ Models/Livro.cs
@@ -6,8 +6,9 @@ public class Livro : ItemBiblioteca
 {
     public Isbn Isbn { get; }
     public int Estoque { get; set; }
-
-    public Livro(string titulo, string autor, string editora, int anoLancamento, int edicao, Isbn isbn, int estoque)
+    public int NumPaginas { get; set; }
+     
+    public Livro(string titulo, string autor, string editora, int anoLancamento, int edicao, string isbn, int estoque, int numPaginas)
     {
         Guarda.RegrasString.ContraParametrosNulosOuVazios(titulo, nameof(titulo));
         Guarda.RegrasString.ContraParametrosNulosOuVazios(autor, nameof(autor));
@@ -16,14 +17,24 @@ public class Livro : ItemBiblioteca
         Guarda.RegrasInt.ContraParametrosNaoPositivos(anoLancamento, nameof(anoLancamento));
         Guarda.RegrasInt.ContraParametrosNaoPositivos(edicao, nameof(edicao));
         Guarda.RegrasInt.ContraParametrosNegativos(estoque, nameof(estoque));
+        Guarda.RegrasInt.ContraParametrosNaoPositivos(numPaginas, nameof(numPaginas));
 
         Titulo = titulo;
         Autor = autor;
         Editora = editora;
         AnoLancamento = anoLancamento;
         Edicao = edicao;
-        Isbn = isbn;
+        Isbn = new Isbn(isbn);
         Estoque = estoque;
+        NumPaginas = numPaginas;
+        PrazoEmprestimo = CalculaPrazoEmprestimo();
+    }
+
+    protected sealed override TimeSpan CalculaPrazoEmprestimo()
+    {
+        var numDias = (int)Math.Ceiling((double)NumPaginas / 25) * 2;
+        var numDiasTimeSpan = TimeSpan.FromDays(numDias);
+        return numDiasTimeSpan;
     }
 
     public override bool Equals(object? obj)
